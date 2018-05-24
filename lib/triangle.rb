b6566de5386b7d415007f71b5e9dfda9f0304d79
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-24 21:22:53 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,11 +2,9 @@ class Triangle
 
 
   def initialize(s1, s2, s3)
-    @s1 = s1
-    @s2 = s2
-    @s3 = s3
-    
-
+    @s1 = s1 #instance variable = arguement
+    @s2 = s2 #instance variable = arguement
+    @s3 = s3 #instance variable = arguement 
   end
 
 end
