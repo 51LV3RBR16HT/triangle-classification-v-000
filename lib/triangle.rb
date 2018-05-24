e5a27dcdd68441df25ea4ff3d15e70ce64d06a80
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-24 21:57:05 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -30,8 +30,8 @@ class Triangle
   end
 end
 
-  class TriangleError < StandardError
-    def message
-      puts "Not a triangle."
-    end
+class TriangleError < StandardError
+  def message
+    puts "Not a triangle."
+  end
 end
