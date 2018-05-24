dfb95846c23ac9ad2af71723421a2183e9f81cac
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-24 21:26:50 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,11 @@
 class Triangle
 
+  attr_accessor :s1, :s2, :s3
 
   def initialize(s1, s2, s3)
     @s1 = s1 #instance variable = arguement
     @s2 = s2 #instance variable = arguement
-    @s3 = s3 #instance variable = arguement 
+    @s3 = s3 #instance variable = arguement
   end
 
 end
