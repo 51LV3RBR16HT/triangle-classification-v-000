abf896858ee42f4d6c21dd459e066e40bd1189bd
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-24 22:00:03 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,9 +3,9 @@ class Triangle
   attr_accessor :s1, :s2, :s3, :kind
 
   def initialize(s1, s2, s3)
-    @s1 = s1 #instance variable = arguement
-    @s2 = s2 #instance variable = arguement
-    @s3 = s3 #instance variable = arguement
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3 
   end
 
   # Triangle rules: The sum of the lengths of any two sides of a triangle
