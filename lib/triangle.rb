63c23ee8b4df2834e9d28bf262ae39b2fe912453
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-24 22:05:21 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,11 +14,6 @@ class Triangle
 
   def kind
     if (s1 * s2 * s3) == 0 || (s1 + s2) <= s3 || (s2 + s3) <= s1 || (s3 + s1) <= s2
-      begin
-        raise TriangleError
-
-        # puts error.message
-      end
     elsif s1 == s2 && s1 == s3
       self.kind = :equilateral
     elsif s1 == s2 || s1 == s3 || s2 == s3
