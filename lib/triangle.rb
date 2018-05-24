303a4fa9fbc4cb71ebe069706c71b2dce09119c8
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-24 22:09:40 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,11 +7,7 @@ class Triangle
     @s2 = s2
     @s3 = s3
   end
-
-  # Triangle rules: The sum of the lengths of any two sides of a triangle
-  # always exceeds the length of the third side. This is a principle known
-  # as the triangle inequality.
-
+  
   def kind
     if (s1 * s2 * s3) == 0 || (s1 + s2) <= s3 || (s2 + s3) <= s1 || (s3 + s1) <= s2
       begin
